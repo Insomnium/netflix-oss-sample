ab7d48f17e4442bb4c3bd94be4451a64fd5216a9
igor petrov <igor_petrov@epam.com>
2017-11-03 17:47:43 +0400

    eureka and config-server bugfix 1. fix dependencies for eureka and config-server 2. register config-server on eureka 3. fix eureka and config-server context configuration 4. update spring-cloud from Dalston.SR2 to Dalston.SR4

--- infra/config-server/src/main/java/com/epam/hw/netflix/ConfigServerApplication.java
+++ infra/config-server/src/main/java/com/epam/hw/netflix/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.epam.hw.netflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigServer
 public class ConfigServerApplication {
 
--- infra/eureka/src/main/java/com/epam/hw/netflix/EurekaServerApplication.java
+++ infra/eureka/src/main/java/com/epam/hw/netflix/EurekaServerApplication.java
@@ -2,10 +2,12 @@ package com.epam.hw.netflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaServerApplication {
     public static void main(String[] args) {
