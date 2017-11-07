0a3d72cba646372975daacbec6983bc46ce5304e
igor petrov <igor_petrov@epam.com>
2017-11-07 18:38:03 +0400

    infra configuration improvements 1. workspaces-api config-server/eureka configuration via bootstrap.yml 2. 'symmetric' config-server profile for symmetric key based encryption 3. prefer-id-address for config-server, eureka and workspaces-api

--- business-services/workspaces-api/src/main/java/com/epam/hw/netflix/WorkplacesApplication.java
+++ business-services/workspaces-api/src/main/java/com/epam/hw/netflix/WorkplacesApplication.java
@@ -2,11 +2,13 @@ package com.epam.hw.netflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class WorkplacesApplication {
 
 	public static void main(String[] args) {
