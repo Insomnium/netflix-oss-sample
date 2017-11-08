667abb923532eab7f44f97e2ce9cac6fd5c9485f
igor petrov <igor_petrov@epam.com>
2017-11-08 17:49:15 +0400

    jacoco test coverage with reports

--- business-services/workspaces-api/src/main/java/com/epam/hw/netflix/repository/DummyWorkspaces.java
+++ business-services/workspaces-api/src/main/java/com/epam/hw/netflix/repository/DummyWorkspaces.java
@@ -1,7 +1,8 @@
 package com.epam.hw.netflix.repository;
 
 import com.epam.hw.netflix.domain.Workspace;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
@@ -9,7 +10,8 @@ import java.util.List;
 
 
 @ConfigurationProperties("workspaces")
-@Data
+@Getter
+@Setter
 @RefreshScope
 public class DummyWorkspaces {
     private List<Workspace> dummy;
