adbb2e5fd0b136dd392f4e04e6953b7316b80499
Igor Petrov <Ins137@gmail.com>
2018-03-20 12:59:18 +0400

    add Step 1: custom link to Github authorization. TODO: facebook, etc.

--- infra/auth/src/main/java/net/ins/hw/netflix/SocialSecurityConfig.java
+++ infra/auth/src/main/java/net/ins/hw/netflix/SocialSecurityConfig.java
@@ -1,10 +1,12 @@
 package net.ins.hw.netflix;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableOAuth2Sso // Exactly on a class that extends WebSecurityConfigurerAdapter!
 public class SocialSecurityConfig extends WebSecurityConfigurerAdapter {
 
 //    @Autowired
@@ -13,10 +15,9 @@ public class SocialSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().and()
                 .antMatcher("/**")
                 .authorizeRequests()
-                    .antMatchers("/", "/login")
+                    .antMatchers("/", "/login**", "/index.html")
                     .permitAll()
                 .anyRequest()
                     .authenticated();
