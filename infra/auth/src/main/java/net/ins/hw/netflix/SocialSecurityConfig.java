10a26ac740427782cd62c46a7abda2f08a2be665
Igor Petrov <Ins137@gmail.com>
2018-03-20 15:00:38 +0400

    manual Oauth2Client configuration

--- infra/auth/src/main/java/net/ins/hw/netflix/SocialSecurityConfig.java
+++ infra/auth/src/main/java/net/ins/hw/netflix/SocialSecurityConfig.java
@@ -2,22 +2,37 @@ package net.ins.hw.netflix;
 
 import net.ins.hw.netflix.config.AuthApplicationProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import javax.servlet.Filter;
+
 @Configuration
-@EnableOAuth2Sso // Exactly on a class that extends WebSecurityConfigurerAdapter!
+@EnableOAuth2Client // Exactly on a class that extends WebSecurityConfigurerAdapter!
 public class SocialSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private AuthApplicationProperties properties;
+
+    @Autowired
+    private OAuth2ClientContext oAuth2ClientContext;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,7 +43,39 @@ public class SocialSecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/", "/login**", "/index.html")
                     .permitAll()
                 .anyRequest()
-                    .authenticated();
+                    .authenticated()
+                .and()
+                    .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
+    private Filter ssoFilter() {
+        OAuth2ClientAuthenticationProcessingFilter githubFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/github");
+        OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(github(), oAuth2ClientContext);
+        githubFilter.setRestTemplate(facebookTemplate);
+        UserInfoTokenServices tokenServices = new UserInfoTokenServices(githubResource().getUserInfoUri(), github().getClientId());
+        tokenServices.setRestTemplate(facebookTemplate);
+        githubFilter.setTokenServices(tokenServices);
+        return githubFilter;
+    }
+
+    @Bean
+    @ConfigurationProperties("github.client")
+    public OAuth2ProtectedResourceDetails github() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("github.resource")
+    public ResourceServerProperties githubResource() {
+        return new ResourceServerProperties();
     }
 
     @Bean
